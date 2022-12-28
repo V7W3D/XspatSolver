78007d491fb0634bf07c55ef22a359ef8e248d0e
salhi younes <younessalhi520@gmail.com>
2022-12-28 12:29:33 -0500

    add search aux

--- src/XpatSolverSearch.ml
+++ src/XpatSolverSearch.ml
@@ -1,3 +1,5 @@
+let max_score = 52
+
 let score state = 
 	let score_aux state index = 
 		match FArray.get state.deposit index with
@@ -16,9 +18,24 @@ module States =
 
 let init_set_states state_init = States.singleton state_init
 
-let get_max_state state_set = States.max_elt state_set
+let get_max_state state_set = States.max_elt_opt state_set
 
-let search_aux move state_set = ()
+let valide_move move state = ()
+
+let set_add_all state move_list = ()
+
+let is_winnig state = if (score state = max_score) then true else false
+
+let rec search_aux move state_set saved_state =
+	match get_max_state state_set with
+		| None -> false
+		| Some s -> match is_winnig s with
+			| true -> true
+			| false -> match valide_move move s with 
+				| None -> search_aux saved_state States.empty
+				| Some l -> let updated_state = States.remove s state_set in
+						let filled_updated_state = set_add_all s l in
+						search_aux move filled_updated_state updated_state
 
 let search move state_init = 
 	let state_set = init_set_states state_init in 
